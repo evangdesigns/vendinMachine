5e935d83170b3fc70793d996e4aa7a0cdefd1ac4
Evan Grabenstein <evangdesigns@gmail.com>
2019-10-29 21:40:08 -0500

    completed login and out

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 import apiKeys from './helpers/apiKeys.json';
+import auth from './components/Auth/auth';
+import authData from './helpers/data/authData';
+import naveyOut from './components/navey/navey';
 import '../styles/main.scss';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
+  authData.checkLoginStaus();
+  auth.loginButton();
+  naveyOut.logoutEvent();
 };
 
 init();
