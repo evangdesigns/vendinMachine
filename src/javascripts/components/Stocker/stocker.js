05a76f0b2f152a198cd6ec2cb52d180f5d6fb65a
Evan Grabenstein <evangdesigns@gmail.com>
2019-11-04 20:20:40 -0600

    updated

--- src/javascripts/components/Stocker/stocker.js
+++ src/javascripts/components/Stocker/stocker.js
@@ -1,7 +1,24 @@
+import $ from 'jquery';
+import firebase from 'firebase/app';
 import smash from '../../helpers/data/smash';
-import './stocker.scss';
+import 'firebase/auth';
 import utilities from '../../helpers/utilities';
 import stockCard from '../StockCard/stockcard';
+import snackPositionData from '../../helpers/data/snackPositionData';
+import './stocker.scss';
+import machine from '../machine/machine';
+
+const deleteFromMachine = (e) => {
+  e.preventDefault();
+  const { uid } = firebase.auth().currentUser;
+  snackPositionData.deleteSnackPosition(e.target.id)
+    .then(() => {
+      // eslint-disable-next-line no-use-before-define
+      buildTheStocker(uid);
+      machine.buildTheMachine();
+    })
+    .catch((error) => console.error(error));
+};
 
 const buildTheStocker = (uid) => {
   smash.getSnacksWithPositions(uid)
@@ -13,6 +30,7 @@ const buildTheStocker = (uid) => {
       });
       domString += '</div>';
       utilities.printToDom('stock', domString);
+      $('#stock').on('click', '.delete-snack-position', deleteFromMachine);
     })
     .catch((error) => console.error(error));
 };
